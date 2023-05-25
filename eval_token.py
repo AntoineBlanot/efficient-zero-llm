aa73de9b4f8ffbaa67c1864a1fc2b35aab09f5c3
MAEDA Chikara <antoineblanot@gmail.com>
2023-05-25 13:51:47 +0900

    train and eval scrpit for token classif

--- eval_token.py
+++ eval_token.py
@@ -5,18 +5,18 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from evaluate import load
-from transformers import HfArgumentParser, BitsAndBytesConfig, Adafactor, RobertaTokenizerFast, get_linear_schedule_with_warmup
-from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training
+from transformers import HfArgumentParser, BitsAndBytesConfig, RobertaTokenizerFast, RobertaConfig
+from peft import PeftModel, PeftConfig
 
 from model.modeling import RobertaForTokenClassification
 from data import DatasetFromDisk, RobertaTokenCollator
-from args import TrainingArgs, ModelArgs, DataArgs
+from args import ModelArgs, DataArgs
 from trainer import Trainer
 
 #region Parser
-parser = HfArgumentParser([TrainingArgs, ModelArgs, DataArgs])
-train_args, model_args, data_args = parser.parse_args_into_dataclasses()
-print(train_args ,model_args, data_args, sep='\n')
+parser = HfArgumentParser([ModelArgs, DataArgs])
+model_args, data_args = parser.parse_args_into_dataclasses()
+print(model_args, data_args, sep='\n')
 #endregion
 
 
@@ -27,22 +27,14 @@ quantization_config = BitsAndBytesConfig(
     load_in_8bit=True,
     llm_int8_skip_modules=trainable_layers
 )
-model = RobertaForTokenClassification.from_pretrained(model_args.pretrained_model_name_or_path, device_map=device_map, quantization_config=quantization_config)
-# Define LoRA Config
-lora_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    target_modules=['query', 'value'],
-    lora_dropout=0.05,
-    bias='none',
-    modules_to_save=trainable_layers
-)
-# prepare int-8 model for training
-model = prepare_model_for_int8_training(model)
-# add LoRA adaptor
-model = get_peft_model(model, lora_config)
-print(model)
-model.print_trainable_parameters()
+peft_config = PeftConfig.from_pretrained(model_args.pretrained_model_name_or_path)
+base_config = RobertaConfig.from_pretrained(model_args.pretrained_model_name_or_path)
+
+model = RobertaForTokenClassification.from_pretrained(pretrained_model_name_or_path=peft_config.base_model_name_or_path, **base_config.to_diff_dict(), quantization_config=quantization_config, device_map=device_map)
+print('Base model loaded')
+model = PeftModel.from_pretrained(model, model_args.pretrained_model_name_or_path, device_map=device_map)
+print('Full checkpoint loaded')
+model.eval()
 #endregion
 
 
@@ -50,28 +42,8 @@ model.print_trainable_parameters()
 tokenizer = RobertaTokenizerFast.from_pretrained(model_args.pretrained_model_name_or_path, model_max_length=data_args.seq_length, add_prefix_space=True)
 data_collator = RobertaTokenCollator(tokenizer, label2id=model.base_model.model.config.label2id)
 
-train_data = DatasetFromDisk(data_args.path + '/train')
 eval_data = DatasetFromDisk(data_args.path + '/dev')
-
-train_dataloader = DataLoader(train_data, batch_size=data_args.bs, collate_fn=data_collator)
 eval_dataloader = DataLoader(eval_data, batch_size=data_args.bs, collate_fn=data_collator)
-#endregion
-
-
-#region Optimizer + LR Scheduler
-optimizer = Adafactor(
-    model.parameters(),
-    lr=train_args.lr,
-    eps=(1e-30, 1e-3),
-    clip_threshold=1.0,
-    decay_rate=-0.8,
-    beta1=None,
-    weight_decay=train_args.wd,
-    relative_step=False,
-    scale_parameter=False,
-    warmup_init=False,
-)  
-scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=train_args.warmup_steps, num_training_steps=train_args.max_steps)
 #endregion
 
 
@@ -114,14 +86,10 @@ def compute_metrics(preds_list: List[torch.Tensor], labels_list: List[torch.Tens
 
 trainer = Trainer(
     model=model,
-    train_dataloader=train_dataloader,
     eval_dataloader=eval_dataloader,
     tokenizer=tokenizer,
-    optimizer=optimizer,
-    scheduler=scheduler,
-    train_args=train_args,
     compute_metrics=compute_metrics
 )
 
-metrics = trainer.train()
+metrics = trainer.eval()
 print(metrics)
