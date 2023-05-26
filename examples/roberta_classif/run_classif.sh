cd86c640ca3da5b70647b1ef8fd6b3a1e8f3b146
MAEDA Chikara <antoineblanot@gmail.com>
2023-05-26 18:20:57 +0900

    run script for seq classif example

--- examples/roberta_classif/run_classif.sh
+++ examples/roberta_classif/run_classif.sh
@@ -9,14 +9,15 @@ accelerate launch --config_file $CONFIG train_classif.py \
   --pretrained_model_name_or_path $MODEL_PATH \
   --path $DATA_PATH --bs 128 --seq_length 128 \
   --output_dir "exp/$RUN_NAME" --lr 1e-4 --wd 0 \
-  --max_steps 7544 --warmup_steps 755 --log_steps 100 --eval_steps 1000 --save_steps 1000 \
+  --max_steps 75450 --warmup_steps 7545 --log_steps 500 --eval_steps 5000 --save_steps 5000 \
   --wandb_project "efficient-llm" --wandb_name $RUN_NAME
 
-# EVAL
-CONFIG="config/single_gpu.yaml"
-MODEL_PATH="/home/chikara/ws/efficient-llm/exp/roberta-nli/checkpoint-7000"
-DATA_PATH="/home/chikara/ws/datasets/examples/NLI"
 
-accelerate launch --config_file $CONFIG eval_classif.py \
-  --pretrained_model_name_or_path $MODEL_PATH \
-  --path $DATA_PATH --bs 32 --seq_length 512
+# # EVAL
+# CONFIG="config/single_gpu.yaml"
+# MODEL_PATH="..."
+# DATA_PATH="/home/chikara/ws/datasets/examples/NLI"
+
+# accelerate launch --config_file $CONFIG eval_classif.py \
+#   --pretrained_model_name_or_path $MODEL_PATH \
+#   --path $DATA_PATH --bs 32 --seq_length 512
