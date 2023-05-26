a3e679d75d776584cb74bf827d9ba2c89a847eb2
MAEDA Chikara <antoineblanot@gmail.com>
2023-05-26 18:21:11 +0900

    run script for tok classif example

--- examples/roberta_token/run_token.sh
+++ examples/roberta_token/run_token.sh
@@ -9,15 +9,15 @@ accelerate launch --config_file $CONFIG train_token.py \
   --pretrained_model_name_or_path $MODEL_PATH \
   --path $DATA_PATH --bs 32 --seq_length 128 \
   --output_dir "exp/$RUN_NAME" --lr 1e-4 --wd 0 \
-  --max_steps 36084 --warmup_steps 3609 --log_steps 100 --eval_steps 1000 --save_steps 1000 \
+  --max_steps 120280 --warmup_steps 12028 --log_steps 500 --eval_steps 5000 --save_steps 5000 \
   --wandb_project "efficient-llm" --wandb_name $RUN_NAME
 
 
-# EVAL
-CONFIG="config/single_gpu.yaml"
-MODEL_PATH="/home/chikara/ws/efficient-llm/exp/roberta-span/checkpoint-36000"
-DATA_PATH="/home/chikara/ws/datasets/examples/SPAN-DETECTION"
+# # EVAL
+# CONFIG="config/single_gpu.yaml"
+# MODEL_PATH="..."
+# DATA_PATH="/home/chikara/ws/datasets/examples/SPAN-DETECTION"
 
-accelerate launch --config_file $CONFIG eval_token.py \
-  --pretrained_model_name_or_path $MODEL_PATH \
-  --path $DATA_PATH --bs 32 --seq_length 512
+# accelerate launch --config_file $CONFIG eval_token.py \
+#   --pretrained_model_name_or_path $MODEL_PATH \
+#   --path $DATA_PATH --bs 32 --seq_length 512
